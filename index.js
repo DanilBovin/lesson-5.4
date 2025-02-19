78834550a925b98d25ec7c8a399117d100306924
Danil Bovin <Danyagame2018@gmail.com>
2025-02-19 21:47:06 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let value = prompt("Введіть число, яке більше одиниці!");
-let numberA = 5 
 
-for (let numberA = 5; numberA <= 8; numberA +=2) {
+for (let value = 5; value <= 5; value++) {
     console.log(value)
 }
